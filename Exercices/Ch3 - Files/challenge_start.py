77f649782af1281342a0b0155bb90e8c1136aa92
pulaswiatek <swiatek.paulinamaria@gmail.com>
2023-02-21 10:13:14 +0100

    no message

--- Exercices/Ch3 - Files/challenge_start.py
+++ Exercices/Ch3 - Files/challenge_start.py
@@ -6,8 +6,8 @@ from os import path
 
 #def main():
 # get a directory
-#src = path.realpath("challenge_start.py")    
-#root_dir, tail = path.split(src)
+src = path.realpath("challenge_start.py")    
+root_dir, tail = path.split(src)
 
 files_list = os.listdir()  # or just () if we want to do it in a current directory
 onlyfiles = [f for f in files_list if os.path.isfile(os.path.join(root_dir, f))]
@@ -15,35 +15,40 @@ onlyfiles = [f for f in files_list if os.path.isfile(os.path.join(root_dir, f))]
 # loop to count total bytes in all the files:
 totalbytes = 0
 for i, filename in enumerate(onlyfiles):
-    filepath = os.path.join(root_dir, filename)
-    totalbytes += path.getsize(filepath)
+    #filepath = os.path.join(root_dir, filename)
+    #totalbytes += path.getsize(filepath)
+    totalbytes += path.getsize(filename)
     print(totalbytes)
 
 # make a new folder
-newpath = os.path.join(root_dir, "results")
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
+# newpath = os.path.join(root_dir, "results")
+# if not os.path.exists(newpath):
+#     os.makedirs(newpath)
+os.mkdir("results")
 
 # make a new .txt files (for results), if it doesnt exist yet
 #resultsfile = open(os.path.join(newpath, "results.txt"), "w+")
 resultsfile = open("results/results.txt", "w+")
 
 # Open the file for appending text to the end
-resultsfile = open(os.path.join(newpath, "results.txt"), "a+" )    
+#resultsfile = open(os.path.join(newpath, "results.txt"), "a+" )    
+resultsfile = open("results/results.txt", "a+" )   
 
 # Write total amount of bytes into the files:
 #Line1 = ["Totatl bytecount:", str(totalbytes), "\n"]
 #resultsfile.writelines(Line1)  # writing multiple strings at a time
-resultsfile.write("Total bytecount: " + str(totalbytes) + "\n")
-resultsfile.write("Files list: \n______________ \n")
+if resultsfile.mode == "a+":
+    resultsfile.write("Total bytecount: " + str(totalbytes) + "\n")
+    resultsfile.write("Files list: \n______________ \n")
     
-# Write the list of files names:
-for i, filename in enumerate(onlyfiles):
-    LineNext = [filename, "\n"]
-    resultsfile.writelines(LineNext)
+    # Write the list of files names:
+    for i, filename in enumerate(onlyfiles):
+        #LineNext = [filename, "\n"]
+        #resultsfile.writelines(LineNext)
+        resultsfile.write(filename + "\n")
 
-# # close the file when done
-resultsfile.close() 
+    # # close the file when done
+    resultsfile.close() 
 
 #if __name__ == "__main__":
 #   main()
